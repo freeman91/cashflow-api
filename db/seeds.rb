41a61fb89331842dca911ad0aeb065a51bb33efa
freeman91 <addisonfreeman91@gmail.com>
2020-05-22 01:07:44 -0400

    script to get cashcode data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,1 @@
 
-# RECREATE USERS AND ACCOUNTS
-User.destroy_all()
-Account.destroy_all()
-users = {
-    'addison@example.com': '12345678'
-}
-
-user.each do |user|
-    user = User.create!(email: 'addison@example.com', password: '12345678')
-    Account.create!(user_id: user.id)
-end
-
-
-# CREATE INITIAL GROUPS FOR ALL USERS
