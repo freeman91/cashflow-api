0ad0d4528e5823648c5c793f623b33f1cedb0492
freeman91 <addisonfreeman91@gmail.com>
2020-06-05 00:57:22 -0400

    started adding dates into the tables, refactor

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -3,9 +3,9 @@ module Api
     class DashboardController < ApiController
       def data
         now = DateTime.now()
-        @expenses = Expense.where(account_id: current_user.accounts.first.id).last(5)
-        @incomes = Income.where(account_id: current_user.accounts.first.id).last(5)
-        @work_hours = WorkHour.where(account_id: current_user.accounts.first.id).last(5)
+        @expenses = Expense.where(account_id: current_user.accounts.first.id, bill: false).last(5).reverse
+        @incomes = Income.where(account_id: current_user.accounts.first.id).last(5).reverse
+        @work_hours = WorkHour.where(account_id: current_user.accounts.first.id).last(5).reverse
         @net_income_year = (Income.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear()).sum(:amount) - Expense.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear()).sum(:amount))
         @net_income_month = (Income.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear(), cwmonth: cwmonth(now.cweek())).sum(:amount) - Expense.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear(), cwmonth: cwmonth(now.cweek())).sum(:amount))
         @net_income_week = (Income.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear(), cweek: now.cweek()).sum(:amount) - Expense.where(account_id: current_user.accounts.first.id, cwyear: now.cwyear(), cweek: now.cweek()).sum(:amount))
