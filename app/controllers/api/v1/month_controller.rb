cb5641fdf607e22fc83fa2543c992f252e55ddf2
freeman91 <addisonfreeman91@gmail.com>
2020-08-14 18:44:57 -0400

    Update to accept date params

--- app/controllers/api/v1/month_controller.rb
+++ app/controllers/api/v1/month_controller.rb
@@ -2,58 +2,56 @@ module Api
   module V1
     class MonthController < ApiController
       def data
-        now = DateTime.now()
-        @week = now.cweek()
-        @month = cwmonth(@week)
-        @year = now.cwyear()
+        week = Integer(params["week"])
+        year = Integer(params["year"])
+        month = cwmonth(week)
 
-        @expenses = Expense.where(account_id: current_user.accounts.first.id, cwyear: @year, cwmonth: @month).order(date: :asc)
-        @incomes = Income.where(account_id: current_user.accounts.first.id, cwyear: @year, cwmonth: @month).order(date: :asc)
-        @work_hours = WorkHour.where(account_id: current_user.accounts.first.id, cwyear: @year, cwmonth: @month).order(date: :asc)
+        expenses = Expense.where(account_id: current_user.accounts.first.id, cwyear: year, cwmonth: month).order(date: :asc)
+        incomes = Income.where(account_id: current_user.accounts.first.id, cwyear: year, cwmonth: month).order(date: :asc)
+        work_hours = WorkHour.where(account_id: current_user.accounts.first.id, cwyear: year, cwmonth: month).order(date: :asc)
 
-        weeks = cweeks(@month)
+        weeks = cweeks(month)
 
-        @monthStats = {}
+        monthStats = {}
         weeks.each { |wk|
-          wkIncTotal = @incomes.where(cweek: wk).sum(:amount)
-          wkExpTotal = @expenses.where(cweek: wk, bill: false).sum(:amount)
-          wkWkhrTotal = @work_hours.where(cweek: wk).sum(:amount)
+          wkIncTotal = incomes.where(cweek: wk).sum(:amount)
+          wkExpTotal = expenses.where(cweek: wk, bill: false).sum(:amount)
+          wkWkhrTotal = work_hours.where(cweek: wk).sum(:amount)
           temp = {}
           temp["net"] = wkIncTotal - wkExpTotal
           temp["expense"] = wkExpTotal
           temp["income"] = wkIncTotal
           temp["work_hours"] = wkWkhrTotal
           temp["wage"] = wkWkhrTotal === 0 ? 0 : wkIncTotal / wkWkhrTotal
-          @monthStats[wk] = temp
+          monthStats[wk] = temp
         }
 
-        @expTotal = @expenses.sum(:amount)
-        @incTotal = @incomes.sum(:amount)
-        @wkhrTotal = @work_hours.sum(:amount)
+        expTotal = expenses.sum(:amount)
+        incTotal = incomes.sum(:amount)
+        wkhrTotal = work_hours.sum(:amount)
 
-        @netincome = @incTotal - @expTotal
+        netincome = incTotal - expTotal
 
         render json: {
                  status: "SUCCESS",
                  message: "Loaded month data",
                  cwdate: {
-                   week: @week,
-                   month: @month,
-                   year: @year,
+                   week: week,
+                   month: month,
+                   year: year,
                  },
-                 monthStats: @monthStats.to_json,
-                 netincome: @netincome,
-                 expTotal: @expTotal,
-                 incTotal: @incTotal,
-                 wkhrTotal: @wkhrTotal,
+                 monthStats: monthStats.to_json,
+                 netincome: netincome,
+                 expTotal: expTotal,
+                 incTotal: incTotal,
+                 wkhrTotal: wkhrTotal,
                }, status: :ok
       end
 
       def bills
-        now = DateTime.now()
-        week = now.cweek()
+        week = Integer(params["week"])
+        year = Integer(params["year"])
         month = cwmonth(week)
-        year = now.cwyear()
         bills = Expense.where(account_id: current_user.accounts.first.id, cwyear: year, cwmonth: month, bill: true).order(date: :asc)
         render json: {
           status: "SUCCESS",
--- app/controllers/api/v1/week_controller.rb
+++ app/controllers/api/v1/week_controller.rb
@@ -2,36 +2,35 @@ module Api
   module V1
     class WeekController < ApiController
       def data
-        now = DateTime.now()
-        @week = now.cweek()
-        @month = cwmonth(@week)
-        @year = now.cwyear()
+        week = Integer(params["week"])
+        year = Integer(params["year"])
+        month = cwmonth(week)
 
-        @expenses = Expense.where(account_id: current_user.accounts.first.id, cwyear: @year, cweek: @week, bill: false).order(date: :asc)
-        @incomes = Income.where(account_id: current_user.accounts.first.id, cwyear: @year, cweek: @week).order(date: :asc)
-        @work_hours = WorkHour.where(account_id: current_user.accounts.first.id, cwyear: @year, cweek: @week).order(date: :asc)
+        expenses = Expense.where(account_id: current_user.accounts.first.id, cwyear: year, cweek: week, bill: false).order(date: :asc)
+        incomes = Income.where(account_id: current_user.accounts.first.id, cwyear: year, cweek: week).order(date: :asc)
+        work_hours = WorkHour.where(account_id: current_user.accounts.first.id, cwyear: year, cweek: week).order(date: :asc)
 
-        @expTotal = @expenses.sum(:amount)
-        @incTotal = @incomes.sum(:amount)
-        @wkhrTotal = @work_hours.sum(:amount)
+        expTotal = expenses.sum(:amount)
+        incTotal = incomes.sum(:amount)
+        wkhrTotal = work_hours.sum(:amount)
 
-        @netincome = @incTotal - @expTotal
+        netincome = incTotal - expTotal
 
         render json: {
                  status: "SUCCESS",
                  message: "Loaded week data",
                  cwdate: {
-                   week: @week,
-                   month: @month,
-                   year: @year,
+                   week: week,
+                   month: month,
+                   year: year,
                  },
-                 netincome: @netincome,
-                 expTotal: @expTotal,
-                 incTotal: @incTotal,
-                 wkhrTotal: @wkhrTotal,
-                 expenses: @expenses,
-                 incomes: @incomes,
-                 work_hours: @work_hours,
+                 netincome: netincome,
+                 expTotal: expTotal,
+                 incTotal: incTotal,
+                 wkhrTotal: wkhrTotal,
+                 expenses: expenses,
+                 incomes: incomes,
+                 work_hours: work_hours,
                }, status: :ok
       end
     end
