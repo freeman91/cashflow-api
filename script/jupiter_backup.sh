023a0c20f7708ed138f1180c94a5e27962db9af1
freeman91 <addisonfreeman91@gmail.com>
2020-08-20 22:08:25 -0400

    Add comments to scripts

--- script/jupiter_backup.sh
+++ script/jupiter_backup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# this script runs through crontab on jupiter once a day @ 3am
+
+# crontab contents: 
+# -------------------------------------------------------------------------------------------
+#
+# PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/Postgres.app/Contents/Versions/latest/bin
+# 0 3 * * * cd ~/production/cashflow && zsh script/jupiter_backup.sh > /tmp/stdout.log 2> /tmp/stderr.log
+#
+# -------------------------------------------------------------------------------------------
+
 pg_dump -v cashflow_development > ~/postgres/backups/cashflow_jupiter_$(date +%Y%m%d).bak
--- script/restore_dev_db.sh
+++ script/restore_dev_db.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+# run this in dev when you want to get the latest db snapshot from jupiter
+
 red="\033[31m"
 yellow="\033[33m"
 green="\033[32m"
