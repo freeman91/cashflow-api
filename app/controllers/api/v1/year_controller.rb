dcd394c6acba687587338c39a2a6e4b6e94ef188
freeman91 <addisonfreeman91@gmail.com>
2021-01-04 20:23:12 -0500

    update year data

--- app/controllers/api/v1/year_controller.rb
+++ app/controllers/api/v1/year_controller.rb
@@ -10,7 +10,7 @@ module Api
         incomes = Income.where(account_id: current_user.accounts.first.id, date: Date.new(year, 1, 1)..Date.new(year, 12, 31)).order(date: :asc)
         work_hours = WorkHour.where(account_id: current_user.accounts.first.id, date: Date.new(year, 1, 1)..Date.new(year, 12, 31)).order(date: :asc)
 
-        yearStats = {}
+        yearStats = []
         months = (1..12).to_a
         months.each do |month|
           monthIncTotal = incomes.where(date: Date.new(year, month, 1)..Date.new(year, month, -1)).sum(:amount)
@@ -22,7 +22,7 @@ module Api
           temp["income"] = monthIncTotal
           temp["work_hours"] = monthWkhrTotal
           temp["wage"] = monthWkhrTotal === 0 ? 0 : monthIncTotal / monthWkhrTotal
-          yearStats[month] = temp
+          yearStats[month - 1] = temp
         end
 
         expenseTotal = expenses.sum(:amount)
