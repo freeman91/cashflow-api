954379d070f85d1b931caa9dc588bb5a93c07192
freeman91 <addisonfreeman91@gmail.com>
2021-01-06 20:28:37 -0500

    refactor

--- app/controllers/api/v1/networth_controller.rb
+++ app/controllers/api/v1/networth_controller.rb
@@ -11,12 +11,13 @@ module Api
         cur_month = month
         cur_year = year
         (1..11).each do |_count|
-          cur_month = cur_month - 1 == 0 ? 12 : cur_month - 1
-          cur_year = cur_month == 12 ? cur_year - 1 : cur_year
           cur_debts_total = Debt.where(account_id: current_user.accounts.first.id, date: Date.new(cur_year, cur_month, 1)..Date.new(cur_year, cur_month, -1)).sum(:amount)
           cur_props_total = Property.where(account_id: current_user.accounts.first.id, date: Date.new(cur_year, cur_month, 1)..Date.new(cur_year, cur_month, -1)).sum(:amount)
           cur_net = cur_props_total - cur_debts_total
           netWorthData.push([cur_month, cur_year, (cur_props_total - cur_debts_total)])
+
+          cur_month = cur_month - 1 == 0 ? 12 : cur_month - 1
+          cur_year = cur_month == 12 ? cur_year - 1 : cur_year
         end
 
         render json: {
